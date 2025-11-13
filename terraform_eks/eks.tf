6ea2f10810190ffccd3179c688e16c5b19618814
vicoldana <55087815+vicoldana@users.noreply.github.com>
2025-11-13 17:49:07 +0200

    Add files via upload

---
+++ terraform_eks/eks.tf
@@ -0,0 +1,23 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "eks-sandbox"
+  cluster_version = "1.30"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+  enable_irsa     = true
+
+  eks_managed_node_groups = {
+    nodes = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 3
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Project = "EKS-Sandbox"
+  }
+}
