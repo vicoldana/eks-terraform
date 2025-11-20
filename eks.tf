23f6172fe101b4daa0af1d822a0d629a206ee3cf
vicoldana <55087815+vicoldana@users.noreply.github.com>
2025-11-20 12:32:58 +0200

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -2,23 +2,24 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "eks-sandbox"
+  cluster_name    = "eks-task3"
   cluster_version = "1.30"
   vpc_id          = module.vpc.vpc_id
-  create_iam_oidc_provider = false
   subnet_ids      = module.vpc.private_subnets
-  enable_irsa     = true
+
+  # Dezactivează OIDC / IRSA pentru a evita eroarea de permisiune
+  enable_irsa = false
 
   eks_managed_node_groups = {
     nodes = {
-      desired_size = 2
-      min_size     = 1
-      max_size     = 3
+      desired_size  = 2
+      min_size      = 1
+      max_size      = 3
       instance_types = ["t3.medium"]
     }
   }
 
   tags = {
-    Project = "EKS-Sandbox"
+    Project = "EKS-Task3"
   }
 }
